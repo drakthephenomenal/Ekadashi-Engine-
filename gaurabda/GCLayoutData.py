a95af5bac2b073a7e58ef14f1d1f0e29c449f931
drakthephenomenal <akthephenomenal@zohomail.com>
2026-06-03 14:52:06 +0600

    Delete gaurabda directory

--- gaurabda/GCLayoutData.py
+++
@@ -1,4 +0,0 @@
-textSizeH1 = 36
-textSizeH2 = 32
-textSizeText = 24
-textSizeNote = 16
--- gaurabda/GCRasi.py
+++
@@ -1,4 +0,0 @@
-from . import GCMath as GCMath
-
-def GetRasi(SunLongitude,Ayanamsa):
-	return int(GCMath.Floor(GCMath.putIn360(SunLongitude - Ayanamsa)/30.0))
